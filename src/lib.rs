c2aa37c8651a5e9b1068941ad43ccd03324e83ed
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2022-10-04 15:11:05 +0200

    Initial commit
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use spin_sdk::{
+    http::{Request, Response},
+    http_component,
+};
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn cloud_start(req: Request) -> Result<Response> {
+    println!("{:?}", req.headers());
+    Ok(http::Response::builder()
+        .status(200)
+        .header("foo", "bar")
+        .body(Some("Hello, from the Fermyon Cloud".into()))?)
+}
